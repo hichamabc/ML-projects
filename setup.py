ecbb74bb34eb0ba16340b4dd7a47b242c54d0e20
hichamabc <hichamamchtk@gmail.com>
2024-01-14 14:10:30 +0100

    test

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_requirements(file_path:str)->List[str]:
 
 setup(
 name='MLproject',
-version='1.0.0',
+version='2.0.0',
 author='Hicham',
 author_email='hichamamchtk@gmail.com',
 packages=find_packages(),
